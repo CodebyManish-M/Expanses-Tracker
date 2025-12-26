14cb409354248441e0dc369c2fe3915c9188ec1f
ManishKumar Mahato <manishkumarmahato2003@gmail.com>
2025-12-27 02:01:55 +0530

    Fix package.json and CRA build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SignUp from "./components/signup";
 import Login from "./components/login"; 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
@@ -8,21 +10,32 @@ import AddExpense from "./components/AddExpense";
 import ManageExpensc from "./components/ManageExpensc";
 import ExpenseReport from "./components/ExpenseReport";
 import ChangePassword from "./components/ChangePassword";
-import './App.css';
+
+import photo1 from "./assets/photo1.jpg";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${photo1})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
+    >
       <BrowserRouter>
-      <NavBar />
+        <NavBar />
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Home/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/addexpense" element={<AddExpense/>} />
-          <Route path="/manageexpense" element={<ManageExpensc/>} />
-          <Route path="/expensereport" element={<ExpenseReport/>} />
-          <Route path="/changepassword" element={<ChangePassword/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addexpense" element={<AddExpense />} />
+          <Route path="/manageexpense" element={<ManageExpensc />} />
+          <Route path="/expensereport" element={<ExpenseReport />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -30,5 +43,3 @@ function App() {
 }
 
 export default App;
-
-
