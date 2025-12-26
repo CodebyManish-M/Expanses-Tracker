440d907e8e9efa372b88a8ed9ea12b41df985e94
ManishKumar Mahato <manishkumarmahato2003@gmail.com>
2025-12-26 15:20:08 +0530

    push the project

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
-
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Expense Tracker API is running 🚀"
+    })
 
 urlpatterns = [
+    path('', home),              # 👈 root URL
     path('admin/', admin.site.urls),
-    path('', include('expenses.urls')),
+    path('api/', include('expenses.urls')),
 ]
